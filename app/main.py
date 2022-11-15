ab34531f39774a16f69c0f519e2b6523ff77a266
cboin1996 <christianboin@hotmail.com>
2022-11-14 20:09:33 -0500

    fix: appease linter

--- app/main.py
+++ app/main.py
@@ -253,7 +253,7 @@ def run(config: settings.SongbirdConfig):
                         "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
                         "Referer": "https://www.youtube.com/",
                         "Accept": "image/avif,image/webp,*/*",
-                        "Host": "www.google.com"
+                        "Host": "www.google.com",
                     }
                     session = web.SimpleSession(
                         "youtube", root_url=config.youtube_home_url, headers=headers
