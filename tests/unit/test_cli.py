bbfd08e306b8678cf0b479762feb5287a1572a44
Christian Boin <christianboin@hotmail.com>
2024-04-14 20:33:36 -0400

    chore: appease linter

--- tests/unit/test_cli.py
+++ tests/unit/test_cli.py
@@ -5,48 +5,49 @@ from io import StringIO
 import os, sys, shutil
 import pytest
 
+
 @pytest.fixture()
 def load_config(monkeypatch, request):
     # below are parametrized vectorized inputs that
     # a user would enter if downloading a song
-    inputs = iter(
-        request.param
-    )
+    inputs = iter(request.param)
     monkeypatch.setattr("builtins.input", lambda _: next(inputs))
     # create tests-output folder
-    tests_data_folder=os.path.join(sys.path[0], "tests-output")
+    tests_data_folder = os.path.join(sys.path[0], "tests-output")
     if not os.path.exists(tests_data_folder):
         os.mkdir(tests_data_folder)
     config = settings.SongbirdCliConfig(
         version=version.version,
         root_path=tests_data_folder,
         gdrive_enabled=False,
-        run_local=True
+        run_local=True,
     )
     # yield setup to test
     yield config
 
-    # test cleanup 
+    # test cleanup
     shutil.rmtree(tests_data_folder)
 
-@pytest.mark.parametrize("load_config",
+
+@pytest.mark.parametrize(
+    "load_config",
     [
         [
-            "jolene", # user enters jolene
-            "0",      # user selects first song
-            "",       # user opts to search youtube
-            "0",      # user selects first song
-            "q"       # user quits
+            "jolene",  # user enters jolene
+            "0",  # user selects first song
+            "",  # user opts to search youtube
+            "0",  # user selects first song
+            "q",  # user quits
         ],
         [
-            "jolene",                                            # user enters jolene
-            "0",                                                 # user selects first song
-            "https://www.youtube.com/watch?v=Ixrje2rXLMA",       # user opts to direct download
-            "q"                                                  # user quits
+            "jolene",  # user enters jolene
+            "0",  # user selects first song
+            "https://www.youtube.com/watch?v=Ixrje2rXLMA",  # user opts to direct download
+            "q",  # user quits
         ],
-    ], 
-    indirect=True
- )
+    ],
+    indirect=True,
+)
 def test_cli_default(load_config):
     # invoke the make function of the cli
     cli.run(config=load_config)
